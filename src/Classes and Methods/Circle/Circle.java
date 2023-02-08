497dcf10c46836dde1d81da88abcc5d4e192381e
AssoDePicche <samuelprado730@gmail.com>
2023-02-08 15:01:33 -0300

    Feat: Add Circle Class Exercise Solution

---
+++ src/Classes and Methods/Circle/Circle.java
@@ -0,0 +1,13 @@
+public class Circle {
+  private static final double PI = 3.14;
+
+  private double radius;
+
+  public Circle(double radius) {
+    this.radius = radius;
+  }
+
+  public double getArea() {
+    return PI * Math.pow(radius, 2);
+  }
+}
