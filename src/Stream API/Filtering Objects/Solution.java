513ffdb83b4c19fd1c7de1017be3ca4263ac0a5c
AssoDePicche <samuelprado730@gmail.com>
2023-02-20 14:32:01 -0300

    Refactor: Move functional interfaces to String Filter class

--- src/Stream API/Filtering Objects/Solution.java
+++ src/Stream API/Filtering Objects/Solution.java
@@ -1,6 +1,3 @@
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,21 +5,11 @@ public class Solution {
   public static void main(String[] args) {
     List<String> list = Arrays.asList("Bulbasaur", "Charmander", "Squirtle", "Caterpie", "Weedle", "Pidgey", "Rattata");
 
-    Predicate<String> filterNamesByLetters = x -> x.contains("a");
-
-    Predicate<String> filterNamesByPrefix = x -> x.startsWith("C");
-
-    Predicate<String> filterNamesBySuffix = x -> x.endsWith("r");
-
-    Function<String, String> toUppercase = x -> x.toUpperCase();
-
-    Consumer<String> printNames = System.out::println;
-
     list.stream()
-        .filter(filterNamesByLetters)
-        .filter(filterNamesByPrefix)
-        .filter(filterNamesBySuffix)
-        .map(toUppercase)
-        .forEach(printNames);
+        .filter(StringFilter.byContent("e"))
+        .filter(StringFilter.byPrefix("P"))
+        .filter(StringFilter.bySuffix("y"))
+        .map(StringFilter.toUpperCase())
+        .forEach(System.out::println);
   }
 }
