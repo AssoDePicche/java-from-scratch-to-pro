db127c28685f09df0a3c2f0a8d919d601c20c924
AssoDePicche <samuelprado730@gmail.com>
2023-02-20 14:19:01 -0300

    Style: Improve code readability

--- src/Stream API/Filtering Objects/Solution.java
+++ src/Stream API/Filtering Objects/Solution.java
@@ -8,15 +8,21 @@ public class Solution {
   public static void main(String[] args) {
     List<String> list = Arrays.asList("Bulbasaur", "Charmander", "Squirtle", "Caterpie", "Weedle", "Pidgey", "Rattata");
 
-    Predicate<String> filterNamesByLetters = x -> x.contains("a"), filterNamesByPrefix = x -> x.startsWith("C"),
-        filterNamesBySuffix = x -> x.endsWith("r");
+    Predicate<String> filterNamesByLetters = x -> x.contains("a");
+
+    Predicate<String> filterNamesByPrefix = x -> x.startsWith("C");
+
+    Predicate<String> filterNamesBySuffix = x -> x.endsWith("r");
 
     Function<String, String> toUppercase = x -> x.toUpperCase();
 
     Consumer<String> printNames = System.out::println;
 
-    list.stream().filter(filterNamesByLetters).filter(filterNamesByPrefix).filter(filterNamesBySuffix).map(toUppercase)
+    list.stream()
+        .filter(filterNamesByLetters)
+        .filter(filterNamesByPrefix)
+        .filter(filterNamesBySuffix)
+        .map(toUppercase)
         .forEach(printNames);
-    ;
   }
 }
