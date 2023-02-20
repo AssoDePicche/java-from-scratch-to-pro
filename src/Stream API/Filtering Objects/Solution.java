657697ca45d4f0ede20b6f6690f612db90478b1a
AssoDePicche <samuelprado730@gmail.com>
2023-02-20 14:00:13 -0300

    Feat: Add Filtering Objects Exercise Solution

---
+++ src/Stream API/Filtering Objects/Solution.java
@@ -0,0 +1,22 @@
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+  public static void main(String[] args) {
+    List<String> list = Arrays.asList("Bulbasaur", "Charmander", "Squirtle", "Caterpie", "Weedle", "Pidgey", "Rattata");
+
+    Predicate<String> filterNamesByLetters = x -> x.contains("a"), filterNamesByPrefix = x -> x.startsWith("C"),
+        filterNamesBySuffix = x -> x.endsWith("r");
+
+    Function<String, String> toUppercase = x -> x.toUpperCase();
+
+    Consumer<String> printNames = System.out::println;
+
+    list.stream().filter(filterNamesByLetters).filter(filterNamesByPrefix).filter(filterNamesBySuffix).map(toUppercase)
+        .forEach(printNames);
+    ;
+  }
+}
